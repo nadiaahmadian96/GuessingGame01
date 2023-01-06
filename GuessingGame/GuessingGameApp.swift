2332f7498ebbff79c22d57efb320c90af32adda9
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2023-01-06 19:51:19 +0330

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Nadia Ahmadian on 2023-01-06.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
